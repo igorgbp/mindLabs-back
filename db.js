9a39604af3ad23b4e963758d7c5e867f7081f383
igorgbp <igorgabriel12bq@gmail.com>
2023-05-29 05:54:44 -0300

    advance

--- db.js
+++ db.js
@@ -1,18 +1,69 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '321321',
-  database: 'mindlabs'
-});
+async function connect() {
+  if (global.connection && global.connection.state !== "disconnected")
+    return global.connection;
 
-connection.connect((error) => {
-  if (error) {
-    console.error('Erro ao conectar ao banco de dados:', error);
-  } else {
-    console.log('Conexão com o banco de dados estabelecida com sucesso!');
-  }
-});
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "baguvix6",
+    database: "mindlabs",
+  });
+  global.connection = connection;
+  return connection;
+}
+connect();
 
-module.exports = connection;
+async function getUser() {
+  const conn = await connect();
+  result = conn.query("select * from user", (error, results) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      // Handle the error appropriately
+    } else {
+      console.log(results);
+      return results
+    }
+  });
+}
+
+async function loginCheck(data) {
+  const conn = await connect();
+  console.log('lgch',data)
+  result = conn.query(`select * from user where username="fff"`, (error, results) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      // Handle the error appropriately
+    } else {
+      console.log('logincheck function')
+      console.log(typeof results);
+      return results
+    }
+  });
+ 
+}
+
+
+async function insertUser(user) {
+  const conn = await connect();
+  console.log(user)
+  result = conn.query(
+    `insert into user values (${user.id},"${user.username}","${user.email}","${user.password}") `,
+    (error, results) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        // Handle the error appropriately
+      } else {
+        // Process the query results
+        console.log("Query results:", results);
+      }
+    }
+  );
+  return user
+
+  console.log("inside funciton");
+  return result;
+}
+
+module.exports = { connection, getUser, insertUser, loginCheck};
