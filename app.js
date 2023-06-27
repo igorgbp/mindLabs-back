efb9b4e65f8731cb41e9064d48f60222a393f621
igorgbp <igorgabriel12bq@gmail.com>
2023-06-27 17:20:46 -0300

    db connection, criação de routes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
 const routes = require("./routes");
+
+// Configurar o CORS
+app.use(cors());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,5 +19,5 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(routes);
 
-
-app.listen(4534, () => console.log("Servidor rodando na porta 4534"));
+const port = 4534;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
