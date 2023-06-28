0628cff0e9614f970021cff0db77702da92cfeeb
igorgbp <igorgabriel12bq@gmail.com>
2023-06-28 05:32:50 -0300

    cgs

--- functions/user.js
+++
@@ -1,9 +0,0 @@
-function cadUser(req,res){
-    async function run (){
-        const file = req.body;
-        console.log("route cad-user");
-        const cad = await insertUser(file);
-        console.log('cad', cad);
-    }
-    run()
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-// const http = require("http");
-
-// http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end(JSON.stringify({
-//         "status": "ok",
-//     }));
-// }).listen(3000, ()=> console.log('server rodando na 3000'))
